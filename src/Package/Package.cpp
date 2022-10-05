bece5e5cd591ff1cb0f662d53178359eac329c6b
HeosFx <flaviantheurel@outlook.fr>
2022-10-05 17:57:49 +0200

    Modification nom de classe

---
+++ src/Package/Package.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file Package.cpp
+ * @author Flavian THEUREL
+ * @brief Class that regroup a value, data type and unit from a data
+ * @version 0.1
+ * @date 2022-10-02
+ */
+
+#include "Package.hpp"
+#include <iomanip>
+
+Package::Package(): m_value(), m_unit()
+{
+  m_type = "No Type";
+}
+
+Package::Package(const Package& dc_p): m_value(dc_p.m_value), m_unit(dc_p.m_unit)
+{
+  m_type = dc_p.m_type;
+}
+
+Package::Package(std::string type_p, std::string unit_p): m_value(), m_unit(unit_p)
+{
+  m_type = type_p;
+}
+
+Package::~Package()
+{
+}
+
+Package& Package::operator=(const Package& dc_p)
+{
+  m_type = dc_p.m_type;
+  m_unit = dc_p.m_unit;
+  m_value = dc_p.m_value;
+  return *this;
+}
+
+std::ostream& operator<<(std::ostream& os_p, const Package& dc_p)
+{
+  return os_p << dc_p.m_type << ": " << dc_p.m_value << dc_p.m_unit << "\t";
+}
---
+++ src/Package/Package.hpp
@@ -0,0 +1,56 @@
+/**
+ * @file Package.hpp
+ * @author Flavian THEUREL
+ * @brief Class that regroup a value, data type and unit from a data
+ * @version 0.1
+ * @date 2022-10-02
+ */
+
+#ifndef PACKAGE_H
+#define PACKAGE_H
+
+#include <string>
+#include <ostream>
+
+/**
+ * @class Package
+ * @brief Class that regroup a value, data type and unit from a data
+ */
+class Package
+{
+private:
+  int m_value; // Value from the sensor
+  std::string m_type; // Data type
+  std::string m_unit; // Data unit
+
+public:
+  Package();
+  Package(const Package& dc_p);
+  ~Package();
+  Package& operator=(const Package& dc_p);
+  
+
+  /**
+   * @brief Construct a new Package object that will be use while initializing it for the first time
+   * 
+   * @param type_p 
+   * @param unit_p 
+   */
+  Package(std::string type_p, std::string unit_p);
+  
+
+  // Friend declaration
+
+
+  // A bouger dans le serveur
+  /**
+   * @brief Organize the datas and informations to print them properly
+   * 
+   * @param os_p Output stream
+   * @param dc_p Data class
+   * @return std::ostream& Output that will be printed
+   */
+  friend std::ostream& operator<<(std::ostream& os_p, const Package& dc_p);
+};
+
+#endif
