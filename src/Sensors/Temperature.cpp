5599d907ea890ab10ac0638b45d4635b36faf069
HeosFx <flaviantheurel@outlook.fr>
2022-10-05 12:51:46 +0200

    Convertion °C en K

--- src/Sensors/Temperature.cpp
+++ src/Sensors/Temperature.cpp
@@ -10,13 +10,13 @@
 #include <random>
 
 // Parameters of the normal distribution
-#define C_MEAN 23
-#define C_DISP 3
+#define C_MEAN 23 //°C
+#define C_DISP 3  //°C
 
 int Temperature::aleaGenVal()
 {
   std::random_device rand_dev; // Initialize the random device
   std::mt19937 rng(rand_dev()); // A Mersenne Twister pseudo-random generator of 32-bit numbers with a state size of 19937 bits
-  std::normal_distribution<std::mt19937::result_type> normal_dist(C_MEAN, C_DISP); // Normal distribution
+  std::normal_distribution<std::mt19937::result_type> normal_dist(C_MEAN+273, C_DISP+273); // Normal distribution
   return normal_dist(rng);
 }
