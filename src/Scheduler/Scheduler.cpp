cc5b8ddef8c36254aefc098a4cacf18975ca6342
HeosFx <flaviantheurel@outlook.fr>
2022-10-09 11:37:46 +0200

    Ajout Scheduler

---
+++ src/Scheduler/Scheduler.cpp
@@ -0,0 +1,65 @@
+/**
+ * @file Scheduler.cpp
+ * @author Flavian THEUREL
+ * @brief Class that trasmits data from the sensors to the server at a predetermined time
+ * @version 0.1
+ * @date 2022-10-05
+ */
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#include "Scheduler.hpp"
+#include <queue>
+#include <iostream>
+
+
+Scheduler::Scheduler(): m_server(), m_humiditySensor(), m_lightSensor(), m_pressureSensor(), m_temperatureSensor()
+{
+}
+
+Scheduler::Scheduler(const Scheduler& sch_p): m_server(sch_p.m_server), m_humiditySensor(sch_p.m_humiditySensor), m_lightSensor(sch_p.m_lightSensor), m_pressureSensor(sch_p.m_pressureSensor), m_temperatureSensor(sch_p.m_temperatureSensor)
+{
+}
+
+Scheduler::~Scheduler()
+{
+}
+
+void Scheduler::StartSimul(int simulSec_p, unsigned int waitingTime_p)
+{
+  std::queue<Package> pkgBuffer;
+
+  m_server.askParameters();
+
+  // Loop until the simulation end
+  while (simulSec_p > 0)
+  {
+    // Get all the datas
+    Package temperaturePkg(m_temperatureSensor.m_type, m_temperatureSensor.m_unit, m_temperatureSensor.getData());
+    pkgBuffer.push(temperaturePkg);
+    Package humidityPkg(m_humiditySensor.m_type, m_humiditySensor.m_unit, m_humiditySensor.getData());
+    pkgBuffer.push(humidityPkg);
+    Package pressurePkg(m_pressureSensor.m_type, m_pressureSensor.m_unit, m_pressureSensor.getData());
+    pkgBuffer.push(pressurePkg);
+    Package lightPkg(m_lightSensor.m_type, m_lightSensor.m_unit, m_lightSensor.getData());
+    pkgBuffer.push(lightPkg);
+
+    // Transmit the packages
+    while (!pkgBuffer.empty())
+    {
+      m_server.receiveData(pkgBuffer.front());
+      pkgBuffer.pop();
+    }
+    
+    // Wait WAIT_TIMER
+    Sleep(waitingTime_p);
+
+    // Reduce the simulation time remaining
+    simulSec_p-=waitingTime_p;
+  }
+  
+}
---
+++ src/Scheduler/Scheduler.hpp
@@ -0,0 +1,50 @@
+/**
+ * @file Scheduler.hpp
+ * @author Flavian THEUREL
+ * @brief Class that trasmits data from the sensors to the server at a predetermined time
+ * @version 0.1
+ * @date 2022-10-05
+ */
+
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include <vector>
+#include "../Server/Server.hpp"
+#include "../Sensors/Sensor.hpp"
+#include "../Sensors/Temperature.hpp"
+#include "../Sensors/Humidity.hpp"
+#include "../Sensors/Pressure.hpp"
+#include "../Sensors/Light.hpp"
+
+// instancier des packets et les envoyer au serveur
+
+/**
+ * @class Scheduler 
+ * @brief Class that trasmits data from the sensors to the server at a predetermined time
+ */
+class Scheduler
+{
+private:
+  Server m_server; // Main server
+  Temperature m_temperatureSensor; // Temperature sensor
+  Humidity m_humiditySensor; // Humidity sensor
+  Pressure m_pressureSensor; // Pressure sensor
+  Light m_lightSensor; // Light sensor
+
+public:
+  Scheduler();
+  Scheduler(const Scheduler& sch_p);
+  ~Scheduler();
+  Scheduler& operator=(const Scheduler& sch_p);
+
+  /**
+   * @brief Do the transmition with a clock
+   * 
+   * @param simulSec_p Total time of the simulation
+   * @param waitingTime_p Time between each data measurement
+   */
+  void StartSimul(int simulSec_p, unsigned int waitingTime_p);
+};
+
+#endif
