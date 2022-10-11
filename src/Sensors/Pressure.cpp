780db02bdb579b05b9260e1c7f0f7de2f96ab4c3
HeosFx <flaviantheurel@outlook.fr>
2022-10-11 08:38:47 +0200

    Correction aleaGenVal avec mersenne twister basé sur le temps de la machine

--- src/Sensors/Pressure.cpp
+++ src/Sensors/Pressure.cpp
@@ -8,15 +8,38 @@
 
 #include "Pressure.hpp"
 #include <random>
+#include <chrono>
 
 // Parameters of the normal distribution
 #define C_MEAN 79289 // Real value in Pa : 79289.71
 #define C_DISP 3447 // Real value in Pa : 3447.3785
 
+Pressure::~Pressure()
+{
+}
+
 int Pressure::aleaGenVal()
 {
-  std::random_device rand_dev; // Initialize the random device
-  std::mt19937 rng(rand_dev()); // A Mersenne Twister pseudo-random generator of 32-bit numbers with a state size of 19937 bits
-  std::normal_distribution<std::mt19937::result_type> normal_dist(C_MEAN, C_DISP);  // Normal distribution
-  return normal_dist(rng);
+  // Initialize the random device
+  std::random_device rd;
+
+  // seed value is designed specifically to make initialization
+  // parameters of std::mt19937 (instance of std::mersenne_twister_engine<>)
+  // different across executions of application
+  std::mt19937::result_type seed = rd() ^ (
+    (std::mt19937::result_type)
+    std::chrono::duration_cast<std::chrono::seconds>(
+        std::chrono::system_clock::now().time_since_epoch()
+        ).count() +
+    (std::mt19937::result_type)
+    std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::high_resolution_clock::now().time_since_epoch()
+        ).count() );
+  
+  // Instantiate a Mersenne Twister pseudo-random generator of 32-bit numbers with a state size of 19937 bits
+  std::mt19937 gen(seed);
+
+  // Setup the normal distribution
+  std::normal_distribution<float> normal_dist(C_MEAN, C_DISP); // Normal distribution
+  return (int)normal_dist(gen);
 }
--- src/Sensors/Temperature.cpp
+++ src/Sensors/Temperature.cpp
@@ -8,15 +8,38 @@
 
 #include "Temperature.hpp"
 #include <random>
+#include <chrono>
 
 // Parameters of the normal distribution
 #define C_MEAN 23 //°C
 #define C_DISP 3  //°C
 
+Temperature::~Temperature()
+{
+}
+
 int Temperature::aleaGenVal()
 {
-  std::random_device rand_dev; // Initialize the random device
-  std::mt19937 rng(rand_dev()); // A Mersenne Twister pseudo-random generator of 32-bit numbers with a state size of 19937 bits
-  std::normal_distribution<std::mt19937::result_type> normal_dist(C_MEAN+273, C_DISP+273); // Normal distribution
-  return normal_dist(rng);
+  // Initialize the random device
+  std::random_device rd;
+
+  // seed value is designed specifically to make initialization
+  // parameters of std::mt19937 (instance of std::mersenne_twister_engine<>)
+  // different across executions of application
+  std::mt19937::result_type seed = rd() ^ (
+    (std::mt19937::result_type)
+    std::chrono::duration_cast<std::chrono::seconds>(
+        std::chrono::system_clock::now().time_since_epoch()
+        ).count() +
+    (std::mt19937::result_type)
+    std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::high_resolution_clock::now().time_since_epoch()
+        ).count() );
+  
+  // Instantiate a Mersenne Twister pseudo-random generator of 32-bit numbers with a state size of 19937 bits
+  std::mt19937 gen(seed);
+
+  // Setup the normal distribution
+  std::normal_distribution<float> normal_dist(C_MEAN, C_DISP); // Normal distribution
+  return (int)(normal_dist(gen) + 273.15f);
 }
