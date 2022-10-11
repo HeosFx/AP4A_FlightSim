57444432cd0d3a154370b9b84ce58cbc02ae5405
HeosFx <flaviantheurel@outlook.fr>
2022-10-11 09:06:12 +0200

    Cin pour demander le temps de simulation

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,18 @@
  */
 
 #include "Scheduler/Scheduler.hpp"
+#include <iostream>
 
 int main()
 {
+  int time_sim;
+  std::cout << "Enter the duration of the simulation in seconds:" << std::endl;
+  std::cin >> time_sim;
+
+  int measurement_period;
+  std::cout << "Enter the measurement period in seconds:" << std::endl;
+  std::cin >> measurement_period;
+
   Scheduler scheduler;
-  scheduler.StartSimul(15, 3);
+  scheduler.StartSimul(time_sim, measurement_period);
 }
