f1c4c34c6301a6d3399180a96f1eae998e7962de
HeosFx <flaviantheurel@outlook.fr>
2022-10-22 18:55:41 +0200

    Correction faute orthographe + Print plus propre

--- src/Server/Server.cpp
+++ src/Server/Server.cpp
@@ -3,13 +3,10 @@
  * @author Flavian THEUREL
  * @brief The server receives datas from captors and displays/saves them
  * @version 0.2
- * @date 2022-10-02
+ * @date 2022-10-22
  */
 
 #include "Server.hpp"
-#include <iostream>
-#include <iomanip>
-#include <fstream>
 
 Server::Server(): m_settingDisplay(true), m_settingLog(true)
 {
@@ -28,69 +25,32 @@ Server& Server::operator=(const Server& serv_p)
   return *this;
 }
 
-void Server::fileWrite(std::string type_p, std::string unit_p, int value_p)
-{
-  std::ofstream m_outfile;
-
-  std::string path = "logs/"+type_p+"_log.txt";
-  // Open the corresponding log file
-  m_outfile.open(path, std::ios::out | std::ios::app);
-  // Print the data from the corresponding sensor
-  m_outfile << type_p << "\t" << value_p << unit_p << std::endl;
-
-  m_outfile.close();
-}
-
-void Server::consolWrite(std::string type_p, std::string unit_p, int value_p)
-{
-  // Print the datas in the console
-  std::cout << type_p << "\t" << value_p << "\t" << unit_p << std::endl;
-
-}
-
-
-void Server::treatment2Package(const Package& package_p)
-{
-  int pValue = package_p.m_value;
-  std::string pType = package_p.m_type;
-  std::string pUnit = package_p.m_unit;
-
-  if (m_settingDisplay == true)
-  {
-    consolWrite(pType, pUnit, pValue);
-  }
-  if (m_settingLog == true)
-  {
-    fileWrite(pType, pUnit, pValue);
-  }
-}
-
-void Server::receiveData(const Package& package_p)
-{
-  treatment2Package(package_p);
-}
-
 void Server::askParameters()
 {
-  
   std::string input;
-  // Ask the user if he want to log the datas
-  std::cout << "Do you want to log the datas? (Y/N)" << std::endl;
+  // Ask the user if he wants to log the data
+  std::cout << "Do you want to log the data? (y/n)" << std::endl;
   std::cin >> input;
-  while (input != "Y" && input != "N")
+  while (input != "y" && input != "n")
   {
-    std::cout << "You entered a wrong answer. Do you want to log the datas? (Y/N)" << std::endl;
+    std::cout << "You entered a wrong answer. Do you want to log the data? (y/n)" << std::endl;
     std::cin >> input;
   }
-  m_settingLog=(input=="Y");
+  m_settingLog=(input=="y");
   
-  // Ask the user if he want to print the datas in the console
-  std::cout << "Do you want to print the datas? (Y/N)" << std::endl;
+  // Ask the user if he wants to print the data in the console
+  std::cout << "Do you want to print the data? (y/n)" << std::endl;
   std::cin >> input;
-  while (input != "Y" && input != "N")
+  while (input != "y" && input != "n")
   {
-    std::cout << "You entered a wrong answer. Do you want to print the datas? (Y/N)" << std::endl;
+    std::cout << "You entered a wrong answer. Do you want to print the data? (y/n)" << std::endl;
     std::cin >> input;
   }
-  m_settingDisplay=(input=="Y");
+  m_settingDisplay=(input=="y");
+
+  if (m_settingDisplay)
+  {
+    std::cout << "Time \t\t\t\t Temperature \t\t Humidity \t\t Pressure \t\t Light\n"
+              << std::left << std::setw(117) << std::setfill('_') << "_" << std::endl;
+  }
 }
--- src/Server/Server.hpp
+++ src/Server/Server.hpp
@@ -3,7 +3,7 @@
  * @author Flavian THEUREL
  * @brief The server receives datas from captors and displays/saves them
  * @version 0.2
- * @date 2022-10-02
+ * @date 2022-10-22
  */
 
 
@@ -11,42 +11,133 @@
 #define SERVER_H
 
 #include <vector>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <ctime>
+#include <map>
 #include "../Package/Package.hpp"
-
-// Faire une FIFO avec la liste
-
 
 /**
  * @class Server
- * @brief Server receives datas from captors and displays/saves them
+ * @brief Server receives data from captors and displays/saves them
  */
 class Server
 {
 private:
-  bool m_settingLog, m_settingDisplay; // Status of the diplaying / logging methods
+  std::map<std::string, int> m_typeMap{{"temperature", 0}, {"humidity", 1}, {"pressure", 2}, {"light",3}}; // Map a data type to an integer
+  bool m_settingLog, m_settingDisplay; // Status of the displaying / logging methods
 
   /**
    * @brief Write in a file the data provided by the sensor
    * 
-   * @param type Data type
-   * @param data_p Data from the sensor
+   * @param type_p Data type
+   * @param unit_p Unit of the measure
+   * @param value_p Value measured from the sensor
+   * @tparam T Type of the variable value
    */
-  void fileWrite(std::string type_p, std::string unit_p, int value_p);
+  template <typename T> void fileWrite(const std::string& type_p, const std::string& unit_p, const T& value_p)
+  {
+    std::ofstream m_outfile;
+
+    std::string path = "logs/"+type_p+"_log.txt";
+    // Open the corresponding log file
+    m_outfile.open(path, std::ios::app | std::ios::out);
+    if (m_outfile.is_open())
+    {
+      // Get the current time
+      time_t time = std::time(0);
+      std::string currTime = std::asctime(std::gmtime(&time));
+      currTime.pop_back(); // Delete \n from the string
+
+      // Print the data from the corresponding sensor
+      m_outfile << currTime << "\t" << type_p << "\t" << value_p << "\t" << unit_p << std::endl;
+      m_outfile.close();
+    }
+  };
 
   /**
    * @brief Write in the console the data provided by a sensor
    * 
-   * @param type Data type
-   * @param data_p Data from the sensor
+   * @param type_p Data type
+   * @param unit_p Unit of the measure
+   * @param value_p Value measured from the sensor
+   * @tparam T Type of the variable value
    */
-  void consolWrite(std::string type_p, std::string unit_p, int value_p);
+  template <typename T> void consoleWrite(const std::string& type_p, const std::string& unit_p, const T& value_p)
+  {
+    // Get the current time
+    time_t time = std::time(0);
+    std::string currTime = std::asctime(std::gmtime(&time));
+    currTime.pop_back(); // Delete \n from the string
+
+    // Print the data in the console
+    // Indent the printing depending on the data type
+    switch (m_typeMap[type_p])
+    {
+      case 0: // Temperature case
+      {
+        std::cout << std::left << std::setw(33) << std::setfill(' ') << currTime << std::left << std::setw(7) << std::setfill('0') << value_p
+                  << " " << unit_p << std::endl;
+      }
+      break;
+
+      case 1: // Humidity case
+      {
+        std::cout << std::left << std::setw(33) << std::setfill(' ') << currTime << std::right << std::setw(24)
+                  << std::setfill(' ') << " " <<  value_p << " " << unit_p << std::endl;
+      }
+      break;
+
+      case 2: // Pressure case
+      {
+        std::cout << std::left << std::setw(33) << std::setfill(' ') << currTime << std::right << std::setw(48)
+                  << std::setfill(' ') << " " << value_p << " " << unit_p << std::endl;
+      }
+      break;
+
+      case 3: // Light case
+      {
+        if (value_p) // Replace 1 by true in the printing
+        {
+          std::cout << std::left << std::setw(33) << std::setfill(' ') << currTime << std::right << std::setw(72)
+                    << std::setfill(' ') << " " << "True" << std::endl;
+        }
+        else // Replace 0 by false in the printing
+        {
+          std::cout << std::left << std::setw(33) << std::setfill(' ') << currTime << std::right << std::setw(72)
+                    << std::setfill(' ') << " " << "False" << std::endl;
+        }
+
+      }
+      break;
+
+      default: // Err case
+        std::cout << std::left << std::setw(33) << std::setfill(' ') << currTime << "No value transferred.";
+    }
+  };
 
   /**
-   * @brief Take the datas from the package
+   * @brief Take the data from the package
    * 
    * @param package_p Package sent by the scheduler
+   * @tparam T Type of the value extracted
    */
-  void treatment2Package(const Package& package_p);
+  template <typename T> void treatment2Package(const Package<T>& package_p)
+  {
+    T pValue = package_p.m_value;
+    std::string pType = package_p.m_type;
+    std::string pUnit = package_p.m_unit;
+
+    if (m_settingDisplay)
+    {
+      consoleWrite<T>(pType, pUnit, pValue);
+    }
+    if (m_settingLog)
+    {
+      fileWrite<T>(pType, pUnit, pValue);
+    }
+  };
 
 public:
   Server();
@@ -54,19 +145,16 @@ public:
   ~Server();
   Server& operator=(const Server& serv_p);
 
-  // /**
-  //  * @brief Do the display / log of the data corresponding to its type (take in account the settings)
-  //  * 
-  //  * @param type Data type
-  //  */
-  // void operator>>(std::string type_p);
-
   /**
    * @brief Retrieve the data and log them
    * 
    * @param package_p Package containing the datas from the sensor
+   * @tparam T Type of the data (float, int, ...)
    */
-  void receiveData(const Package& package_p);
+  template <typename T> void receiveData(const Package<T>& package_p)
+  {
+    treatment2Package<T>(package_p);
+  };
 
   /**
    * @brief Ask the user to enter the parameter of the logging and printing
